34c456f0c08eaa571cd19b67648fcd07507390f3
Enrico Matricardi <enricomatricardi@hotmail.com>
2020-02-27 18:58:01 +0100

    js-jq-hamburger

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$("header .fas.fa-bars").click(function(){
+    $(".hamburger-menu").addClass("active");
+});
+
+$("header .close").click(function(){
+    $(".hamburger-menu").removeClass("active");
+});
